a8dcbaf3e2018c9cee096ef9c7a2c1ced2b7f064
sami sabir-idrissi <samisabiridrissi@icloud.com>
2024-11-26 18:44:44 -0500

    first commit

---
+++ src/main/java/com/ai/cloud/arcade/ai_api_db_arcade/AiApiDbArcadeApplication.java
@@ -0,0 +1,13 @@
+package com.ai.cloud.arcade.ai_api_db_arcade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiApiDbArcadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiApiDbArcadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/cloud/arcade/ai_api_db_arcade/AiApiDbArcadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.cloud.arcade.ai_api_db_arcade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiApiDbArcadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
